8d1e0a1e921a41ebbb4f4406e595767457d8d833
Jacob Escoto <Jescoto046@student.glendale.edu>
2026-08-14 20:40:12 -0700

    established all components of my vm

--- JacobVM.h
+++ JacobVM.h
@@ -1,6 +1,13 @@
+#include <cstdint>
+ 
+ struct JacobVM {
+    uint16_t memory[65536];
+    uint16_t reg[8];
+    uint16_t pc;
+    uint16_t flag; //keeping one flag reg for now
+    //dont know yet if i should use 1-3
+    uint16_t stack[16];
+    uint16_t sp;
+};
 
-
-
-typedef struct {
-
-} JacobVM;
+JacobVM VM = {};
