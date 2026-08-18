c47137f506a735bdb64cb1b3738d233dcdbc32b0
Jacob Escoto <Jescoto046@student.glendale.edu>
2026-08-17 21:37:49 -0700

    Added prototype instructions of ADD & SUB

--- JacobVM.h
+++ JacobVM.h
@@ -5,8 +5,8 @@
 
 class JacobVM {
     public: 
-        std::array<uint16_t, 65536> memory{};
-        std::array<uint16_t, 8> reg{};
+        std::array<uint16_t, 65536> memory = {};
+        std::array<uint16_t, 8> reg = {};
         std::stack<uint16_t> stack;
         uint16_t pc;
         uint16_t flag;
--- main.cpp
+++ main.cpp
@@ -18,15 +18,27 @@ int main() {
     
 
     //grab the opcode 
-    uint16_t opcode = VM.memory[VM.pc];
+    uint16_t opcode = VM.memory[VM.pc];   
     VM.pc++;
+    std::cout << "opcode: " << opcode << std::endl;
 
     uint8_t op = (opcode >> 12) & 0x0F; 
+    uint8_t X = (opcode >> 8) & 0x0F; //the first register 
+    uint8_t Y = (opcode >> 4) & 0x0F; //the second register 
     //need the first number to determine type of instruction
 
     switch (op) 
     {
-        case 0:
+        case 0: //ADD
+        VM.reg[X] = VM.reg[X] + VM.reg[Y];
+        if (VM.reg[X] < 255) { VM.flag = 1; } else { VM.flag = 0;}
+        break;
+
+        case 1: //SUB
+        VM.reg[X] = VM.reg[X] - VM.reg[Y];
+        break;
+
+        case 2: //AND
         break;
     }
 
